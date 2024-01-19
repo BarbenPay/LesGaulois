7a544b20dd0f68039bca0e2140add3e3fec54e7c
BarbenPay <jean-baptiste.salanie@univ-tlse3.fr>
2024-01-19 08:40:42 +0100

    TP1 fin de l'exercice 1

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,21 @@
+package personnages;
+
+public class Gaulois {
+
+	String name;
+	
+	int strength;
+	
+	public Gaulois(String name, int strength) {
+		
+		this.name = name;
+		
+		this.strength = strength;
+		
+	}
+	
+	public String getName(){
+		return(name);
+	}
+	
+}
